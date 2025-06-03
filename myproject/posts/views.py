7aaf644c898daddcf6b7f2e401100b595c400501
Manya <manyaverma0154@gmail.com>
2025-06-03 16:34:17 +0530

    Pages, URLs & slugs, gitignore

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Post
 # Create your views here.
 
@@ -6,3 +7,7 @@ from .models import Post
 def posts_list(request):
     posts = Post.objects.all().order_by('-date')
     return render(request, 'posts/posts_list.html', {'posts': posts})
+
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/post_page.html', {'post': post})
